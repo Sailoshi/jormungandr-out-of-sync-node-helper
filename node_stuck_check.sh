b051f80240296283db1194e8bff022d952cd69c8
Phillip Sailer <nelly-@web.de>
2019-12-20 10:27:56 +0100

    added retrying to the script before restarting node.

--- node_stuck_check.sh
+++ node_stuck_check.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 while true
 do
 RED='\033[0;31m'
@@ -6,6 +8,20 @@ NC='\033[0m' # No Color
 . ~/.bash_profile
 lastBlockHash=`stats | head -n 6 | tail -n 1 | awk '{print $2}'`
 lastBlockCount=`stats | head -n 7 | tail -n 1 | awk '{print $2}' | tr -d \"`
+sleep 5
+tries=10
+counter=0
+while [[ $counter -le $tries ]]
+do
+	shelleyExplorerJson=`curl -X POST -H "Content-Type: application/json" --data '{"query":" query { block (id:\"'$lastBlockHash'\") { id date { slot epoch { id firstBlock { id } lastBlock { id } totalBlocks } } transactions { totalCount edges { node { id block { id date { slot epoch { id firstBlock { id } lastBlock { id } totalBlocks } } leader { __typename ... on Pool { id blocks { totalCount } registration { startValidity managementThreshold owners operators rewards { fixed ratio { numerator denominator } maxLimit } rewardAccount { id } } } } } inputs { amount address { id } } outputs { amount address { id } } } cursor } } previousBlock { id } chainLength leader { __typename ... on Pool { id blocks { totalCount } registration { startValidity managementThreshold owners operators rewards { fixed ratio { numerator denominator } maxLimit } rewardAccount { id } } } } } } "}' https://explorer.incentivized-testnet.iohkdev.io/explorer/graphql 2> /dev/null`
+	shelleyLastBlockCount=`echo $shelleyExplorerJson | grep -o '"chainLength":"[^"]*' | cut -d'"' -f4`
+	if [[ $shelleyLastBlockCount -ne "" ]]; then
+		break
+	fi
+	counter=$(($counter+1))
+	echo "INVALID RESULT. RETRYING..."
+	sleep 3
+done
 shelleyExplorerJson=`curl -X POST -H "Content-Type: application/json" --data '{"query":" query { block (id:\"'$lastBlockHash'\") { id date { slot epoch { id firstBlock { id } lastBlock { id } totalBlocks } } transactions { totalCount edges { node { id block { id date { slot epoch { id firstBlock { id } lastBlock { id } totalBlocks } } leader { __typename ... on Pool { id blocks { totalCount } registration { startValidity managementThreshold owners operators rewards { fixed ratio { numerator denominator } maxLimit } rewardAccount { id } } } } } inputs { amount address { id } } outputs { amount address { id } } } cursor } } previousBlock { id } chainLength leader { __typename ... on Pool { id blocks { totalCount } registration { startValidity managementThreshold owners operators rewards { fixed ratio { numerator denominator } maxLimit } rewardAccount { id } } } } } } "}' https://explorer.incentivized-testnet.iohkdev.io/explorer/graphql`
 shelleyLastBlockCount=`echo $shelleyExplorerJson | grep -o '"chainLength":"[^"]*' | cut -d'"' -f4`
 
@@ -18,16 +34,13 @@ echo "DeltaCount: " $deltaBlockCount
 
 if [[ $(echo $shelleyExplorerJson | grep -o '"message":"[^"]*' | cut -d'"' -f4) == *"Couldn't find block's contents in explorer"* || deltaBlockCount > maximumBlockDeltaCount ]]; then
  echo -e ${RED}"Block was not found within main chain. Please restart your node and remove your current chain cache."${NC}
- echo "Node is out of sync at block " $lastBlockCount >> logs/node-checker-warnings.out
-# echo "Trying to restart the node..."
- ### If you want to restart your node when the blockchain is out of sync, please remove the following uncomments.
- ### And remove/uncomment the exit command.
-# stop
-# rm -r ./mnt
-# start_leader
-# sleep 120 
- exit
+ echo "Node was out of sync at block " $lastBlockCount >> logs/node-checker-warnings.out
+ echo "Trying to restart the node..."
+ stop
+ rm -r mnt
+ start_leader
+ sleep 180
 fi
 
-sleep 300
+sleep 900
 done
